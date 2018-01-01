8be8a2aba88ade2528b5dc92a7e6195449bdb2e5
mikeli <mikemobiledeveloper@gmail.com>
2018-01-01 08:46:56 -0500

    test done

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,13 +1,40 @@
 package com.londonappbrewery.magiceightball;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends Activity {
+
+    ImageView ballDisplay;
+    Button myButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ballDisplay = findViewById(R.id.image_eightBall);
+        myButton = findViewById(R.id.askButton);
+
+        final int[] ballArray = {R.drawable.ball1, R.drawable.ball2, R.drawable.ball3, R.drawable.ball4, R.drawable.ball5};
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomGenerator = new Random();
+
+                int number = randomGenerator.nextInt(6);
+
+                ballDisplay.setImageResource(ballArray[number]);
+            }
+        });
     }
 }
